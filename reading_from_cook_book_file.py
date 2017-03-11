ccca19b6af7f8716caaa3b9cee3833ec0ef6ade9
Estafius <estafius@gmail.com>
2017-03-11 18:54:52 +0200

    Add files via upload

--- reading_from_cook_book_file.py
+++ reading_from_cook_book_file.py
@@ -38,7 +38,9 @@ def print_shop_list(shop_list):
 def create_shop_list():
     person_count = int(input('Введите количество человек: '))
     dishes = input('Введите блюда в расчете на одного человека (через запятую): ').split(', ')
-    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    print('Рецепт из файла: ')
+    cook_book = cook_book_from_file()
+    shop_list = get_shop_list_by_dishes(dishes, person_count,cook_book)
     print_shop_list(shop_list)
 
 
